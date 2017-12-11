daacdce6d9376b07caa6adda0a1a5e96b5f073f1
git4rockyt <tiwarirakesh@gmail.com>
2017-12-11 16:49:00 -0600

    Update initdb.sh

--- initdb.sh
+++ initdb.sh
@@ -1,12 +1,14 @@
 sudo geth --identity "MyTestNetNode" --nodiscover --networkid 1999 --datadir ./chaindata  init ./CustomGenesis.json
 if [ $? -ne 0 ]; then 
-	echo "Error! Cannot Contnue"
+	echo "Error! Cannot Contnue with geth"
+	exit -1
 fi
 
 
 echo "Setting first block"
 sudo geth --password /home/ubuntu/password.txt account new --datadir /home/ubuntu/chaindata > /home/ubuntu/acctaddr.txt
 if [ $? -ne 0 ]; then 
-	echo "Error! Cannot Contnue"
+	echo "Error! Cannot Contnue with setting account"
+	exit -1
 fi
 
