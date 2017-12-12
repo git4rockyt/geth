308c009f62f23e17c86044d4dc09cc6655d36104
Tiwari <rtiwari@c02vd1eng8wl.ds.global>
2017-12-11 21:44:04 -0600

    changed the installer

--- first_block_mining.sh
+++ first_block_mining.sh
@@ -22,10 +22,10 @@ a="first"
 while [ "$a" == "first" ]; do 
 	echo "1" > /home/ubuntu/test1.txt; 
 	sleep 10; 
-	a="$(awk -F\"{|}\" '{print $2}' /home/ubuntu/acctaddr.txt)"; 
+	a=$(awk -F"{|}" '{print $2}' /home/ubuntu/acctaddr.txt)"; 
 	echo "$a" > /home/ubuntu/addr.txt; 
 done
-f [ $? -ne 0 ]; then 
+if [ $? -ne 0 ]; then 
 	echo "Error Cannot Contnue"
 fi
 
