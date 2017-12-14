150e8bdbd077c35b7668aad4d01995ec393a8348
Srikar, Nuvvula <nuvvula.srikar@transamerica.com>
2017-12-14 10:29:02 -0600

    updated rpcapi parameters

--- reinitdb.sh
+++ reinitdb.sh
@@ -2,7 +2,7 @@
 
 LOG_FILE=$1
 
-if [ -z $LOG_FILE ]; then 
+if [ -z $LOG_FILE ]; then
 	LOG_FILE="ethereum-install-`date +%Y-%b-%d-%H-%M-%S`.log"
 fi
 
@@ -20,13 +20,12 @@ function ELOG ( ) {
 
 LOG "Running reinit of geth"
 geth --identity "MyTestNetNode" --nodiscover --networkid 1999 --datadir /home/ubuntu/chaindata  init /home/ubuntu/CustomGenesis.json
-if [ $? -ne 0 ]; then 
+if [ $? -ne 0 ]; then
 	ELOG "Cannot Contnue with reinit of geth"
 fi
 
 LOG "Starting geth with rpc an dbind addr of 0.0.0.0"
-geth --identity "MyTestNetNode" --datadir /home/ubuntu/chaindata --nodiscover --networkid 1999 --rpc --rpcaddr 0.0.0.0 --rpccorsdomain "*" --rpcapi eth,web3,personal >> gethconsole.log 2>&1 &
-if [ $? -ne 0 ]; then 
+geth --identity "MyTestNetNode" --datadir /home/ubuntu/chaindata --nodiscover --networkid 1999 --rpc --rpcaddr 0.0.0.0 --rpccorsdomain "*" --rpcapi db,eth,net,web3,personal >> gethconsole.log 2>&1 &
+if [ $? -ne 0 ]; then
 	ELOG "Cannot start running geth"
 fi
-
